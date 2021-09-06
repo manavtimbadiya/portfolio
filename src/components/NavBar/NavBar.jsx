7d6268c8b8a54c0454b2502afac53c6c6d8d3e69
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-09-06 10:38:57 -0500

    Link Component Added for Client-side routing

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,25 +1,36 @@
 import React from "react";
 import { slide as Menu } from 'react-burger-menu';
+import { Link } from "react-router-dom";
 
 import "./NavBar.css";
 
 class NavBar extends React.Component {
 	render(){
 		return (
-	      <Menu>
-	      {/* We NEVER want to use <a> inside our react router application */}
-    	    <a id="home" className="menu-item" href="/">
+		<Menu>
+	      	{/* We NEVER want to use <a> inside our react router application */}
+			{	/* Because It refresh whole page But we want to use client side routing It doesn't refresh whole page */}
+    		{/* So React-router-dom provide Link component */}
+    	    {/*a id="home" className="menu-item" href="/">
     	    	Home
-    	    </a>
-        	<a id="about" className="menu-item" href="/about">
-        		About
-        	</a>
-        	<a id="projects" className="menu-item" href="/projects">
-        		Projects
-        	</a>
-
-	      </Menu>
-    	);
+    	    </a */}
+    	    <Link to="/" className="menu-item">
+    	    	Home
+    	    </Link>
+    	    <Link to="/about" className="menu-item">
+    	    	About
+    	    </Link>
+    	    <Link to="/projects" className="menu-item">
+    	    	Projects
+    	    </Link>
+    	    <Link to="/skills" className="menu-item">
+    	    	Skills
+    	    </Link>
+    	    <Link to="/contact" className="menu-item">
+    	    	Contact
+    	    </Link>
+		</Menu>
+		);
 	}
 }
 
