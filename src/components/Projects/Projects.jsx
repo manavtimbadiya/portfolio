5a76afdf58a601190c329cadf74cb4a965784378
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-09-05 21:38:01 -0500

    Portfolio Using React

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Projects.css";
+
+const Projects = () => 
+{
+	return (
+		<div>
+			<h1>This is Project page!</h1>
+		</div>
+		);
+};
+
+export default Projects;
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Skills.css";
+
+const Skills = () => {
+	return (
+		<div>
+			<h1>This is the Skills page!</h1>
+		</div>
+		);
+};
+
+export default Skills;
