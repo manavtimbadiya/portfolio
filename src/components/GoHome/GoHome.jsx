6209a2ab5893e974ab59dffb456552990354c08f
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-09-06 19:14:46 -0500

    Programatic Navigation Updated and Styled

--- src/components/GoHome/GoHome.jsx
+++ src/components/GoHome/GoHome.jsx
@@ -1,35 +1,38 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-import HomeIconWhite from "./../../assets/home_white.png";
-import HomeIconBlack from "./../../assets/home_black.png";
-import "./GoHome.css";
+import homeIconWhite from './../../assets/home_white.png';
+import homeIconBlack from './../../assets/home_black.png';
+import './GoHome.css';
 
 // This component is only there to teach you programmatic navigation
 // Note* - It could have been done easily just by using the Link component,
 // But, I've used the history object, to represent it's usage
-
 class GoHome extends Component {
-	navigateToHome = () => {
-		const {history} = this.props;
-		//console.log(history);
-		history.push("/");
-	}
-	render() {
-		const {location} = this.props;
-		const whiteBtn = location.pathname === "/";
-		return(
-			<button onClick={this.navigateToHome}> 
-				<img
-				 className={`go-home-btn ${
-				 	whiteBtn ? "white-bkg" : "black-bkg"
-				 }`}
-				 src = {whiteBtn ? HomeIconBlack : HomeIconWhite}
-				 alt = "home-icon"
-				 />
-			</button>
-			);
-	}
+    navigateToHome = () => {
+        const { history } = this.props;
+        history.push('/');
+    };
+
+    render() {
+        const { location } = this.props;
+        const whiteBtn = location.pathname === '/';
+
+        return (
+            <button
+                className={`go-home-btn ${
+                    whiteBtn ? 'white-bkg' : 'gradient-bkg'
+                }`}
+                onClick={this.navigateToHome}
+            >
+                <img
+                    className='home-icon'
+                    src={whiteBtn ? homeIconBlack : homeIconWhite}
+                    alt='home-icon'
+                />
+            </button>
+        );
+    }
 }
 
 export default withRouter(GoHome);
