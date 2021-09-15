2579e586379b8030e0e191c67aa44fcab2fb4862
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-09-14 23:28:30 -0500

    Portal Updated

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,7 +5,36 @@ import "./Modal.css";
 const Modal = (props) => {
 	return (
         ReactDOM.createPortal( <div className="modal-overlay">
-                <h1>I am the modal!</h1>
+            <div className="modal-container">
+                 <div className='upper-section'>
+                    <h1>Huge Annoucement!</h1>
+                    <p  className='close'>
+                        &times;
+                    </p>
+                </div>
+            </div>
+             <div className='middle-section'>
+                    <h3>React Mega Tutorial is LIVE!</h3>
+                    <p>Do you wish to learn React?</p>
+                    <p>
+                        I've published a completely FREE react project, check
+                        it out today!
+                    </p>
+
+                </div>
+                 <div className='lower-section'>
+                    <a  className='modal-btn btn-red'>
+                        Close
+                    </a>
+                    <a
+                        href='http://theleanprogrammer.com/aam/'
+                        target='_blank'
+                        rel='noreferrer'
+                        className='modal-btn btn-green'
+                    >
+                        Start tutorial
+                    </a>
+                </div> 
             </div>,
                 document.getElementById("modal"))
         ); 
