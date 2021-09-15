9761301dbe1ee551cbaf4018afc86d39a1f28e93
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-09-15 17:08:00 -0500

    Vector Frame updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import Header from '../Header/Header';
+import aboutVector from "./../../assets/about_vector.png";
 import "./About.css";
 
 const About = () => {
 	return(
 			<div className="section-container">
-				<h1>I am the about page!</h1>
+				<Header 
+					heading="About Me."
+					details="Pre-Final year || Computer Engineer studying at ADIT"
+				/>
+			{/* Vector Frame! */}
+				<div className="vector-frame">
+					<img src={aboutVector} className="about-container" alt="about" />
+				</div>
 			</div>
 		)
 }
