231207b2bf136baa20c94f821fa5373bae0c1341
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-09-17 18:32:43 -0500

    About Component Updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,7 @@ import Header from '../Header/Header';
 import FooterLink from "../FooterLink/FooterLink";
 import aboutVector from "./../../assets/about_vector.png";
 import "./About.css";
+import aboutAnime from "./../../assets/about_anime.gif";
 
 const About = () => {
 	return(
@@ -11,6 +12,22 @@ const About = () => {
 					heading="About Me."
 					details="Pre-Final year || Computer Engineer studying at ADIT"
 				/>
+
+				<div className="about-main">
+					<div className="about-left">
+						{/* Sub Section */}
+						<h2 className="about-sub">
+							Student
+						</h2>
+						<p className="about-details">
+							Web Developer from India. Would love to discuss on C, C++, HTML, CSS, ReactJS.
+						</p>
+					</div>
+					
+					<div className="about-right">
+						<img src={aboutAnime} alt="animation" className="about-anime" /> 
+					</div>
+				</div>
 
 				<FooterLink
 					phrase="Check out my "
