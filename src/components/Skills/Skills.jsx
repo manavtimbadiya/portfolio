618e784f1e321e748ef7e18a7cb42fd6317cac16
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-10-18 15:26:09 -0500

    Skills Component Updated

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import Header from "../Header/Header";
+import FooterLink from "../FooterLink/FooterLink";
+import {skillList} from "./../../assets/skillsData";
 import "./Skills.css";
+import skillsVector from "./../../assets/skills_vector.png";
+import SkillsCard from "./SkillsCard";
 
 const Skills = () => {
 	return (
 		<div className="section-container">
-			<h1>This is the Skills page!</h1>
+			<Header 
+				heading='My Skills.'
+                details="Passionate about new technologies, I keep exploring stuff."
+            />
+
+            <div className="skill-card-container">
+            	{
+                    skillList.map(({skillName, skillUrl}) => (
+                         <SkillsCard skillName={skillName} skillUrl={skillUrl} />  
+                    ))
+                }
+            </div>
+
+            <FooterLink 
+            	phrase='Get in ' 
+            	link='touch.' 
+            	toAddress='/contact' 
+            />
+
+            <div className="skills-vector-frame">
+            	<img src={skillsVector} alt="skills" className="skills-vector" />
+            </div>
 		</div>
 		);
 };
