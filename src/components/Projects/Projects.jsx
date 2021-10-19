e33bb0f8aeb0b201f8ec107606088a703b2a2d82
Manav <19cp.manavtimbadiya@adit.ac.in>
2021-10-19 09:52:15 -0500

    ProjectCard Added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FooterLink from '../FooterLink/FooterLink';
 import Header from "../Header/Header";
+import { projectsData } from './../../assets/projectsData';
+import ProjectCard from "./ProjectCard";
 import "./Projects.css";
 
 const Projects = () => 
@@ -11,7 +13,29 @@ const Projects = () =>
                 heading='My Projects.'
                 details="Here are a few cool things I've worked on, do check them out!"
             />
-                <FooterLink
+
+             <div className='project-cards-container'>
+                 {
+                    projectsData.map(
+                        ({
+                        projectName,
+                        projectDescription,
+                        imageUrl,
+                        videoUrl,
+                        projectUrl,
+                    }) => {
+                        return <ProjectCard 
+                                projectName={projectName}
+                                projectDescription={projectDescription}
+                                projectUrl={projectUrl}
+                                imageUrl={imageUrl}
+                                videoUrl={videoUrl}
+                         />
+                    })
+                 }
+             </div>
+
+            <FooterLink
                 phrase='Check out '
                 link='my skills!'
                 toAddress="/skills"
